8409f8e640d82be65d0eb11cd10b179f1e68caeb
abhisheksiddhu <abhisheksiddhu@outlook.in>
2022-01-09 16:37:19 +0530

    tag helpers, model binding, validations

---
+++ NET 6/WebApplication-MVC/Controllers/DataController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApplication_MVC.Models;
+
+namespace WebApplication_MVC.Controllers
+{
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Data Index";
+            ViewBag.CallType = "Get Call";
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            ViewBag.CallType = "Post Call";
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Tag()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Tag(User user)
+        {
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult FormBind()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost, ActionName("FormBind")]
+        public IActionResult FormBindPost()
+        {
+            User user = new User();
+            user.Name = Request.Form["name"];
+            user.Email = Request.Form["email"];
+            user.UserName = Request.Form["username"];
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult FileBind()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileBind(User user)
+        {
+            var file = new FileInfo($"SavedFiles/{DateTime.Now.Ticks}-{user.Photo.FileName}");
+            using (var stream = file.OpenWrite())
+            {
+                await user.Photo.OpenReadStream().CopyToAsync(stream);
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult CustomBind()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult CustomBind([Bind("Name,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.WasFormValid = "Valid";
+            }
+            else
+            {
+                ViewBag.WasFormValid = "Invalid";
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult ModelUpdate()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ModelUpdate(User user)
+        {
+            if (string.IsNullOrWhiteSpace(user.Email))
+            {
+                user.Email = "testemail";
+            }
+            await TryUpdateModelAsync(user);
+            ModelState.Clear();
+            if (TryValidateModel(user))
+            {
+                ViewBag.WasFormValid = "Valid";
+            }
+            else
+            {
+                ViewBag.WasFormValid = "Invalid";
+            }
+            return View(user);
+        }
+    }
+}
---
+++ NET 6/WebApplication-MVC/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_MVC.Models
+{
+    public record User
+    {        
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        [Required,EmailAddress]        
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? UserName { get; set; }
+        public IFormFile? Photo { get; set; }
+    }
+}
